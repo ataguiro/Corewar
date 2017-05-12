723fbd0c65bee85300782b7788a1103821734d2a
Stephane LE LIEGARD <sle-lieg@e3r13p12.42.fr>
2017-05-12 17:19:06 +0200

    dossiers instructions crees, plus qu' a les remplir

--- corewar/src/db_show_map.c
+++ corewar/src/db_show_map.c
@@ -12,61 +12,61 @@
 
 #include "corewar.h"
 
-// static char	*l_hex(unsigned char n)
-// {
-// 	static char	ret[2];
+static char	*l_hex(unsigned char n)
+{
+	static char	ret[2];
 
-// 	ret[0] = '0';
-// 	ret[1] = '0';
+	ret[0] = '0';
+	ret[1] = '0';
 
-// 	ret[0] = n >> 4;
-// 	if (ret[0] <= 9)
-// 		ret[0] += '0';
-// 	else
-// 		ret[0] += 'a' - 10;
-// 	ret[1] = n & 0xf;
-// 	if (ret[1] <= 9)
-// 		ret[1] += '0';
-// 	else
-// 		ret[1] += 'a' - 10;
-// 	return (ret);
-// }
+	ret[0] = n >> 4;
+	if (ret[0] <= 9)
+		ret[0] += '0';
+	else
+		ret[0] += 'a' - 10;
+	ret[1] = n & 0xf;
+	if (ret[1] <= 9)
+		ret[1] += '0';
+	else
+		ret[1] += 'a' - 10;
+	return (ret);
+}
 
-// static void	l_print_classic(unsigned char player, char c, char *tab)
-// {
-// 	(void)tab;
-// 	if (player == '1')
-// 		ft_strcat(tab, GREEN);
-// 	else if (player == '2')
-// 		ft_strcat(tab, BLUE);
-// 	else if (player == '3')
-// 		ft_strcat(tab, RED);
-// 	else if (player == '4')
-// 		ft_strcat(tab, CYAN);
-// 	else
-// 		ft_strcat(tab, GREY);
-// 	ft_strcat(tab, l_hex(c));
-// 	ft_strcat(tab, DEFAULT);
-// 	ft_strcat(tab, " ");
-// }
+static void	l_print_classic(unsigned char player, char c, char *tab)
+{
+	(void)tab;
+	if (player == '1')
+		ft_strcat(tab, GREEN);
+	else if (player == '2')
+		ft_strcat(tab, BLUE);
+	else if (player == '3')
+		ft_strcat(tab, RED);
+	else if (player == '4')
+		ft_strcat(tab, CYAN);
+	else
+		ft_strcat(tab, GREY);
+	ft_strcat(tab, l_hex(c));
+	ft_strcat(tab, DEFAULT);
+	ft_strcat(tab, " ");
+}
 
-// static void	l_print_cursor(unsigned char player, char c, char *tab)
-// {
-// 	(void)tab;
-// 	if (player == '1')
-// 		ft_strcat(tab, GREEN_CURSOR);
-// 	else if (player == '2')
-// 		ft_strcat(tab, BLUE_CURSOR);
-// 	else if (player == '3')
-// 		ft_strcat(tab, RED_CURSOR);
-// 	else if (player == '4')
-// 		ft_strcat(tab, CYAN_CURSOR);
-// 	else
-// 		ft_strcat(tab, GREY_CURSOR);
-// 	ft_strcat(tab, l_hex(c));
-// 	ft_strcat(tab, DEFAULT);
-// 	ft_strcat(tab, " ");
-// }
+static void	l_print_cursor(unsigned char player, char c, char *tab)
+{
+	(void)tab;
+	if (player == '1')
+		ft_strcat(tab, GREEN_CURSOR);
+	else if (player == '2')
+		ft_strcat(tab, BLUE_CURSOR);
+	else if (player == '3')
+		ft_strcat(tab, RED_CURSOR);
+	else if (player == '4')
+		ft_strcat(tab, CYAN_CURSOR);
+	else
+		ft_strcat(tab, GREY_CURSOR);
+	ft_strcat(tab, l_hex(c));
+	ft_strcat(tab, DEFAULT);
+	ft_strcat(tab, " ");
+}
 
 static unsigned char	l_cmp_cursor(t_player *player, size_t target)
 {

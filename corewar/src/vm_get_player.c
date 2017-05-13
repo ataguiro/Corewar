f9ef2f0d3b9610a9173920bf7dcdd425a19055f6
Stephane LE LIEGARD <sle-lieg@e3r3p5.42.fr>
2017-05-13 23:08:40 +0200

    plop

--- corewar/src/vm_get_player.c
+++ corewar/src/vm_get_player.c
@@ -35,6 +35,37 @@ static int	l_rev_int(char *str, size_t len)
 ** str = |magic|prog_name|prog_size|empty|champion code|
 */
 
+static int 	nb_is_free(int nb_to_check)
+{
+	t_player *tmp;
+
+	tmp = g_env.player;
+	while (tmp)
+	{
+		if (tmp->number == nb_to_check)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
+void 		vm_get_nbplayer(void)
+{
+	static int nb_player = 1;
+
+	if (g_env.cmd & NB_PLAY)
+	{
+		g_env.player->number = g_env.option_nb_play;
+		g_env.cmd ^= NB_PLAY;	
+	}
+	else
+	{
+		while (!nb_is_free(nb_player))
+			nb_player++;
+		g_env.player->number = nb_player++;
+	}	
+}
+
 t_header	vm_get_player(char *str)
 {
 	t_header	header;

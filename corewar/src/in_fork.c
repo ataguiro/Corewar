bdc9c7ac5a2071dbc01c1a9d92fa748ff6ecd270
folkowic <folkowic@student.42.fr>
2017-05-15 14:40:35 +0200

    add in_fork

--- corewar/src/in_fork.c
+++ corewar/src/in_fork.c
@@ -6,7 +6,7 @@
 /*   By: sle-lieg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/12 16:36:38 by sle-lieg          #+#    #+#             */
-/*   Updated: 2017/05/15 14:08:59 by folkowic         ###   ########.fr       */
+/*   Updated: 2017/05/15 14:35:19 by folkowic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 void in_fork(t_player *player)
 {
-	t_player	*n;
 	size_t		curs;
 	int			value;
 
 	curs = (player->pc + 1) % MEM_SIZE;
 	value = vm_get_param_val(curs, 2) % IDX_MOD;
-	vm_lst_add_new(&player);
-	ft_memcpy(n, player, sizeof(*player));
-	n->str = ft_strdup(player->str);
+	vm_lst_add_cpy(&player);
 	player->pc = (player->pc + 3) % MEM_SIZE;
 	player->next->pc = (player->next->pc + value) % MEM_SIZE;
-/*	for (t_player *play = g_env.player ; play ; play = play->next)
-		db_show_reg(play);*/
+	int num = 0;
+	for (t_player *play = g_env.player ; play ; play = play->next)
+	{
+		ft_printf("num %d\n", num++);
+		db_show_reg(play);
+	}
 }
--- corewar/src/vm_runtime.c
+++ corewar/src/vm_runtime.c
@@ -6,7 +6,7 @@
 /*   By: sle-lieg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/11 16:36:18 by sle-lieg          #+#    #+#             */
-/*   Updated: 2017/05/15 13:52:27 by folkowic         ###   ########.fr       */
+/*   Updated: 2017/05/15 14:28:00 by folkowic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static void 	l_init_cycle_size()
 {
 	g_env.cycles_size[0] = 0;
-	g_env.cycles_size[1] = 10;
+	g_env.cycles_size[1] = 1;//10;
 	g_env.cycles_size[2] = 5;
 	g_env.cycles_size[3] = 5;
 	g_env.cycles_size[4] = 10;
@@ -26,7 +26,7 @@ static void 	l_init_cycle_size()
 	g_env.cycles_size[9] = 20;
 	g_env.cycles_size[10] = 25;
 	g_env.cycles_size[11] = 25;
-	g_env.cycles_size[12] = 15;//800;
+	g_env.cycles_size[12] = 5;//800;
 	g_env.cycles_size[13] = 10;
 	g_env.cycles_size[14] = 50;
 	g_env.cycles_size[15] = 1000;

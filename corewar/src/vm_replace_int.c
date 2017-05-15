61e49fd25e0597c7d3497336c50d959487f85135
Stephane LE LIEGARD <sle-lieg@e3r5p13.42.fr>
2017-05-15 18:29:00 +0200

    st fonctionnel

--- corewar/src/vm_replace_int.c
+++ corewar/src/vm_replace_int.c
@@ -1,28 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vm_st.c                                            :+:      :+:    :+:   */
+/*   vm_replace_int.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sle-lieg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/05/12 16:35:21 by sle-lieg          #+#    #+#             */
-/*   Updated: 2017/05/12 16:35:22 by sle-lieg         ###   ########.fr       */
+/*   Created: 2017/05/15 18:22:08 by sle-lieg          #+#    #+#             */
+/*   Updated: 2017/05/15 18:22:09 by sle-lieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void vm_st(t_player *player)
+void 	vm_replace_int(unsigned int offset, unsigned int val)
 {
-	t_decode *args;
-	size_t curs;
-	
-	curs = (player->pc + 1) % MEM_SIZE;
-	args = vm_decode_octet(g_env.map.str[curs++]);
-	vm_get_arg(args, &curs);
-	if (args->param2 == 1 && args->arg2 < 17)
-		player->reg[args->arg2] = player->reg[args->arg1];
-	else if (args->param2 == 2)
-		vm_replace_int(player->pc + (args->arg2 % IDX_MOD), player->reg[args->arg1]);
-	player->pc = curs % MEM_SIZE;
+	*(g_env.map.str + (offset % MEM_SIZE)) = val >> 24;
+	*(g_env.map.str + (offset + 1 % MEM_SIZE)) = val << 8 >> 24;
+	*(g_env.map.str + (offset + 2 % MEM_SIZE)) = val << 16 >> 24;
+	*(g_env.map.str + (offset + 3 % MEM_SIZE)) = val << 24 >> 24;
 }
---
+++ samples/champs/st.s
@@ -0,0 +1,8 @@
+.name "st"
+.comment ""
+
+ld %3, r2
+ld %144, r3
+st r2, r5
+st r3, r7
+fork %10

7227f67dcb5fb9f5d85c8f1178ca284907a2b2d1
Stephane LE LIEGARD <sle-lieg@e3r5p13.42.fr>
2017-05-15 16:46:33 +0200

    st ajoutee, a tester

--- corewar/src/in_st.c
+++ corewar/src/in_st.c
@@ -14,6 +14,21 @@
 
 void vm_st(t_player *player)
 {
-	(void)player;
+	t_decode *args;
+	size_t curs;
+	
+	curs = (player->pc + 1) % MEM_SIZE;
+	args = vm_decode_octet(g_env.map.str[curs++]);
+	vm_get_arg(args, &curs);
 
+	/*
+		si arg2 == registre ET arg2 < 17 -> copier reg[arg1] dans reg[arg2]
+		si arg2 == indirect -> PC + (arg2 % IDX_MOD) == reg[arg1]
+	*/
+
+	if (args->param2 == 1 && args->arg2 < 17)
+		player->reg[args->arg2] = player->reg[args->arg1];
+	else if (args->param2 == 2)
+		player->pc = (player->pc + (args->arg2 % IDX_MOD)) % MEM_SIZE;
+	player->pc = curs % MEM_SIZE;
 }
---
+++ samples/champs/st.s
@@ -0,0 +1,8 @@
+.name "st"
+.comment ""
+
+ld %3, r2
+ld %144, r3
+st r2, 10
+st r3, 9
+fork %10

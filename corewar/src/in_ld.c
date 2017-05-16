cc66d5d5a988d922541374cd19faf6b9f726dc5f
Stephane LE LIEGARD <sle-lieg@e3r6p3.42.fr>
2017-05-16 15:48:58 +0200

    ld modified, indirect arg1 functionnal

--- corewar/src/in_ld.c
+++ corewar/src/in_ld.c
@@ -21,7 +21,7 @@ void	in_ld(t_player *player)
 	args = vm_decode_octet(g_env.map.str[curs++]);
 	vm_get_arg(args, &curs);
 	if (args->param1 == IND_SIZE)
-		args->arg1 = vm_get_param_val(player->pc + (args->arg1 % IDX_MODE), 4);
+		args->arg1 = vm_get_param_val(player->pc + (args->arg1 % IDX_MOD), 4);
 	if (args->arg2 > 0 && args->arg2 < 17)
 	{
 		player->carry = args->arg1 ? false : true;

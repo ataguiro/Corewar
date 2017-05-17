29353c1e808c3590f0eda59e5d352fa55313c5f5
folkowic <folkowic@student.42.fr>
2017-05-17 19:00:24 +0200

    add show area

--- corewar/src/in_st.c
+++ corewar/src/in_st.c
@@ -6,7 +6,7 @@
 /*   By: sle-lieg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/12 16:35:21 by sle-lieg          #+#    #+#             */
-/*   Updated: 2017/05/15 19:32:10 by folkowic         ###   ########.fr       */
+/*   Updated: 2017/05/17 18:57:03 by folkowic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,10 @@ void in_st(t_player *player)
 							args->arg1 > 0 && args->arg1 < 17)
 		player->reg[args->arg2] = player->reg[args->arg1];
 	else if (args->param2 == 2 && args->arg1 > 0 && args->arg1 < 17)
-		vm_replace_int(player->pc + (args->arg2 % IDX_MOD), player->reg[args->arg1]);
+	{
+		vm_replace_int(player->pc + (args->arg2 % IDX_MOD),
+				player->reg[args->arg1]);
+		vm_color_area(player->pc + (args->arg2 % IDX_MOD), 4, player->number);
+	}
 	player->pc = curs % MEM_SIZE;
 }

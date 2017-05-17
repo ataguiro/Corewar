8b6eb3695160ef0092d64176101306f850062989
Adam TAGUIROV <ataguiro@e3r5p7.42.fr>
2017-05-17 19:46:08 +0200

    little mistake left

--- asm/barriere.s
+++ asm/barriere.s
@@ -1,4 +1,4 @@
-.name "Barriere lol"
+.name "Barriere"
 .comment "Envoie des torpilles a l'avant et se protege avec des barrieres a l'avant et a l'arriere"
 
 		and	r6, %0, r6
@@ -56,7 +56,7 @@ live_live:	live	%42
 		sti	r1, %:live144, r9	
 		sti	r1, %:live146, r9	
 		sti	r1, %:live148, r9	
-		xor	r9,r10, r1
+		xor	r9,%6,r10
 		zjmp	%:fork_live
 		ld	%6,r9
 		and	r6,%0,r6
--- asm/src/lexer_parser/tools/tl_dir_translate.c
+++ asm/src/lexer_parser/tools/tl_dir_translate.c
@@ -6,7 +6,7 @@
 /*   By: ataguiro <ataguiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/15 11:33:12 by ataguiro          #+#    #+#             */
-/*   Updated: 2017/05/17 16:11:01 by ataguiro         ###   ########.fr       */
+/*   Updated: 2017/05/17 18:31:43 by ataguiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,6 @@ void	tl_dir_translate(char *dir, int *p)
 	}
 	else
 		using = ft_itoa(g_offtab[g_offset_index++].offset);
-	ft_printf("[DEBUG] %s - %s - %d - %d\n", dir, using, g_offtab[g_offset_index - 1].offset, g_offset_index - 1);
 	t == '+' ? last = ft_atoi(using) + ft_atoi(op + 1) : 0;
 	t == '-' ? last = ft_atoi(using) - ft_atoi(op + 1) : 0;
 	t == '*' ? last = ft_atoi(using) * ft_atoi(op + 1) : 0;

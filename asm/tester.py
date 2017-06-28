1d253059af819a40f36de9546897714102c19974
Adam TAGUIROV <ataguiro@e2r7p9.42.fr>
2017-06-28 18:00:07 +0200

    tester.py

---
+++ asm/tester.py
@@ -0,0 +1,26 @@
+from subprocess import Popen, PIPE, check_output, call
+from pwn import *
+import signal
+import os
+import sys
+
+def pc_handler(signum, frame):
+        raise Exception("timeout")
+
+signal.signal(signal.SIGALRM, pc_handler)
+
+if len(sys.argv) != 2:
+    print "Usage : python tester.py [file_to_randomize]"
+    sys.exit(1)
+
+while 1:
+    signal.alarm(5)
+    try:
+        os.system("cat " + sys.argv[1] + " | radamsa > test.s")
+        out = call(["./asm", "test.s"])
+    except:
+        pass
+    print "RET : %s" % str(out)
+    if out > 1 or out < 0:
+        print "SEGFAULT !"
+        sys.exit(1)

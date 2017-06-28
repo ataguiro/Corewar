3633bc0d195745bd6b39dc1764cb25fb069deaf2
folkowic <folkowic@student.42.fr>
2017-06-28 14:47:03 +0200

    pouet

--- corewar/test.s
+++ corewar/test.s
@@ -1,4 +1,4 @@
-.name		"CelebrationFunebrev0.99pl42"
+.name		"Celebration Funebre v0.99pl42"
 .comment	"Jour J"
 
 		ld   %0,r3		
@@ -6,17 +6,6 @@ label:		zjmp %:avantdebut
 		zjmp %0				# pour eviter ecrasement
 						# de l'instruction suivante
 code:		live %42			# 5
-		sti  r15,%0,r14			# 6
-		zjmp %-200			# 3
-		live %42			# 5
-		sti  r15,%0,r14			# 6
-		zjmp %-200			# 3
-		live %42			# 5
-		sti  r15,%0,r14			# 6
-		zjmp %-200			# 3
-		live %42			# 5
-		sti  r15,%0,r14			# 6
-		zjmp %-200			# 3
 		sti  r15,%0,r14			# 6
 		zjmp %-200			# 3
 		sti  r15,%0,r14			# 6
